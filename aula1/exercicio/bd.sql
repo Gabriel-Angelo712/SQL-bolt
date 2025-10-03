aacd1ba322cffd3c68580fd001f9576bea2764e0
GabrielAngelo <gabrielangelo200367@gmail.com>
2025-10-03 10:16:55 +0100

    Terminando a aula 1

---
+++ aula1/exercicio/bd.sql
@@ -0,0 +1,38 @@
+-- CRIAR A BASSE DE DADOS
+
+CREATE database `sql_bolt`;
+
+-- Se a base de dados já estiver criada:
+
+use `sql_bolt`;
+
+-- Definir a tabela "filmes_pixar" que será usada neste caso
+
+CREATE table filmes_pixar(
+    id int auto_increment primary key,
+    title varchar(255),
+    diretor varchar(100),
+    ano int, 
+    duracao_minutos int 
+);
+
+-- Cadastrar os dados nabase tabela
+
+INSERT into filmes_pixar VALUES(
+	(1, 'toy story', 'João Lassater', 1995, 81),
+	(2, 'Vida de inseto', 'João Lasseter', 1998, 95),
+    (3, 'Toy story 2', 'João Lassater', 1999, 93),
+    (4, 'Monstros S.A', 'Peter Docter', 2001, 92),
+    (5, 'Procurando Nemo', 'André Stanton', 2003, 107),
+	(6, 'Os incríveis', 'Brad Bird', 2004, 116),
+    (7, 'carros', 'João Lassater', 2006, 117)
+);
+
+-- 1. Encontrar o title de cada filme
+SELECT title from filmes_pixar;
+-- 2. Encontrar o diretor de cada filme
+SELECT diretor from filmes_pixar;
+-- 1. Encontrar o diretor e o title de cada filme
+SELECT diretor, title from filmes_pixar;
+-- 1. Encontrar o todos os dados de cada filme
+SELECT * from filmes_pixar;
