347e2076fa979ae503bd45ad64a3c9490544713f
GabrielAngelo <gabrielangelo200367@gmail.com>
2025-10-04 14:19:58 +0100

    atualizando o exercício da aula 1

--- aula1/exercicio/bd.sql
+++ aula1/exercicio/bd.sql
@@ -18,15 +18,22 @@ CREATE table filmes_pixar(
 
 -- Cadastrar os dados nabase tabela
 
-INSERT into filmes_pixar VALUES(
+INSERT INTO `filmes_pixar` (`id`, `title`, `diretor`, `ano`, `duracao`) VALUES
 	(1, 'toy story', 'João Lassater', 1995, 81),
 	(2, 'Vida de inseto', 'João Lasseter', 1998, 95),
     (3, 'Toy story 2', 'João Lassater', 1999, 93),
     (4, 'Monstros S.A', 'Peter Docter', 2001, 92),
     (5, 'Procurando Nemo', 'André Stanton', 2003, 107),
 	(6, 'Os incríveis', 'Brad Bird', 2004, 116),
-    (7, 'carros', 'João Lassater', 2006, 117)
-);
+    (7, 'carros', 'João Lassater', 2006, 117),
+    (8, 'Ratatouille', 'Brad Bird', 2007, 115),
+    (9, 'WALL-E', 'André Stanton', 2008, 104),
+    (10, 'Up', 'Peter Docter', 2009, 101),
+    (11, 'Toy story 3', 'Lee Unkrich', 2010, 103),
+    (12, 'Carros 2', 'John Lasseter', 2011, 120),
+    (13, 'Valente', 'Brenda Chapman', 2012, 102),
+    (14, 'Universidade Monstros', 'Dan Scanlon', 2013, 110),
+;
 
 -- 1. Encontrar o title de cada filme
 SELECT title from filmes_pixar;
